56c20830a0b006fc59f29b3a0ecb679c4201de9f
Daniel Yu <88413927+danielyu2003@users.noreply.github.com>
2024-03-05 02:03:30 -0500

    Initial Commit

---
+++ MotionLoop/MotionLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionLoopApp.swift
+//  MotionLoop
+//
+//  Created by Daniel Yu on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MotionLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
