0bb7b210e845aa4d7427df1f075909fbcbd18073
Daniel Yu <yudaniel316@icloud.com>
2024-03-24 00:37:34 -0400

    Refactored view and model, added sliders for Y and Z thresholds

--- MotionLoop/ContentView.swift
+++ MotionLoop/ContentView.swift
@@ -9,29 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
 	@Bindable private var motion = Motion()
+	@State var playing = false
     var body: some View {
         VStack {
-			Text(String(reflecting: motion.accelData))
 			Spacer()
-			HStack {
-				Button("Start") {
-					motion.startDeviceMotion()
+			Text(stringify(motion.accelData))
+			.font(.title)
+			Spacer()
+			Group {
+				if playing {
+					Button("Stop", systemImage: "stop.circle") {
+						motion.stopDeviceMotion()
+						playing.toggle()
+					}
 				}
-				Spacer()
-				Button("Stop") {
-					motion.stopDeviceMotion()
+				else {
+					Button("Start", systemImage: "play.circle") {
+						motion.startDeviceMotion()
+						playing.toggle()
+					}
 				}
 			}
+			.labelStyle(.iconOnly)
+			.font(.largeTitle)
 			Spacer()
-			Slider(
-				value: $motion.threshold,
-				in: 0...1,
-				step: 0.01
-			)
-			Text(String(motion.threshold))
+			HStack {
+				Text("X")
+				Slider(
+					value: $motion.thresholds.xThreshold,
+					in: 0...1,
+					step: 0.01
+				)
+			}
+			Text(String(motion.thresholds.xThreshold))
+			HStack {
+				Text("Y")
+				Slider(
+					value: $motion.thresholds.yThreshold,
+					in: 0...1,
+					step: 0.01
+				)
+			}
+			Text(String(motion.thresholds.yThreshold))
+			HStack {
+				Text("Z")
+				Slider(
+					value: $motion.thresholds.zThreshold,
+					in: 0...1,
+					step: 0.01
+				)
+			}
+			Text(String(motion.thresholds.zThreshold))
         }
         .padding()
     }
+	private func stringify(_ data: (x: Double, y: Double, z: Double)?) -> String {
+		return String(reflecting: data)
+	}
 }
 
 #Preview {
--- MotionLoop/MotionModel.swift
+++ MotionLoop/MotionModel.swift
@@ -12,35 +12,44 @@ import UIKit
 	private let manager = CMMotionManager()
 	private var available: Bool { manager.isDeviceMotionAvailable }
 	private var active: Bool { manager.isDeviceMotionActive }
-	var threshold: Double = 0
 	private(set) var accelData: (x: Double, y: Double, z: Double)? = nil
+	var thresholds: (xThreshold: Double, yThreshold: Double, zThreshold: Double) = (0.0,0.0,0.0)
 	func startDeviceMotion() {
 		if available && !active {
 			manager.deviceMotionUpdateInterval = 1/6
 			manager.showsDeviceMovementDisplay = true
 			manager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
-			let timer = Timer(fire: Date(), interval: (1/6), repeats: true) { timer in
-				if !self.active { timer.invalidate() }
-				if let data = self.manager.deviceMotion {
-					// include frame number and timestamp?
-					let x = data.userAcceleration.x
-					let y = data.userAcceleration.y
-					let z = data.userAcceleration.z
-					self.accelData = (x, y, z)
-					// DEBUG
-					if x > self.threshold && x != 0  {
-						UIImpactFeedbackGenerator(style: .medium).impactOccurred()
-					}
-				}
-			}
-			RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+			RunLoop.current.add(timer(), forMode: RunLoop.Mode.default)
 		}
 	}
 	func stopDeviceMotion() {
 		manager.stopDeviceMotionUpdates()
 		accelData = nil
 	}
-	func setThreshhold(at value: Double) {
-		threshold = value
+	private func timer() -> Timer {
+		return Timer(fire: Date(), interval: (1/6), repeats: true) { timer in
+			if !self.active { timer.invalidate() }
+			if let data = self.manager.deviceMotion {
+				let x = data.userAcceleration.x
+				let y = data.userAcceleration.y
+				let z = data.userAcceleration.z
+				self.handleAccelData(x, y, z)
+			}
+		}
+	}
+	private func handleAccelData(_ x: Double, _ y: Double, _ z: Double) {
+		// Use motion data below
+		// include frame number and timestamp?
+		self.accelData = (x, y, z)
+		// DEBUG
+		if abs(x) > self.thresholds.xThreshold && self.thresholds.xThreshold != 0.0  {
+			UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+		}
+		if abs(y) > self.thresholds.yThreshold && self.thresholds.yThreshold != 0.0  {
+			UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+		}
+		if abs(z) > self.thresholds.zThreshold && self.thresholds.zThreshold != 0.0  {
+			UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+		}
 	}
 }
